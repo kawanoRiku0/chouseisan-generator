dd0132bcd4bc943c0a1390792834db12d45f3a1d
kawanoRiku0 <riku.kawano20020518@gmail.com>
2023-06-07 21:26:27 +0900

    refactor: Change page name & favicon

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import {LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs'
 import 'dayjs/locale/ja';
 import Content from "./components/Content";
+import {useEffect} from "react";
 
 
 function App() {
+    useEffect(() => {
+        document.title = '調整さんジェネレーター';
+    }, []);
     return (
         <div style={{margin: '0 auto', maxWidth: '500px'}}>
             <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ja">
